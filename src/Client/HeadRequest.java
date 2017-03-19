0c4d114dee962fffada8ef09cc3f95df48e6523d
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2017-03-19 15:50:33 +0100

    Opkuis HEAD

--- src/Client/HeadRequest.java
+++ src/Client/HeadRequest.java
@@ -6,27 +6,29 @@ public class HeadRequest {
 
 	public static void head(String[] address, int port) throws UnknownHostException, IOException {
 
-		String newAddress;
 		String hostaddress = address[0];
 		String afterSlash = address[1];
-		
+
 		//verbinding leggen met site.
 		Socket s = new Socket(hostaddress, port);
 		//request
 		PrintWriter pw = new PrintWriter(s.getOutputStream());
-		pw.println("HEAD "+afterSlash+" HTTP/1.1");
-		pw.println("Host: "+hostaddress);
+		pw.println("HEAD " + afterSlash + " HTTP/1.1");
+		pw.println("Host: " + hostaddress);
 		pw.println("");
 		pw.flush();
+		
 		//respons
 		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
 		
 		//printwriter to htmlfile
-		PrintWriter out = new PrintWriter("HEAD"+afterSlash+".html");
+		PrintWriter out = new PrintWriter("HEAD" + hostaddress + ".html");
 		String commandLineString;
-		
+
 		//printen respons
 		while((commandLineString = br.readLine()) != null){
+			System.out.println(commandLineString);
 			out.println(commandLineString);
 			out.flush();
 		}
@@ -39,5 +41,4 @@ public class HeadRequest {
 		pw.close();
 		s.close();
 	}
-
 }
