46f464f50371773ce0abda1cb17961adc2efbf02
Arne Vlietinck <arne.vlietinck@student.kuleuven.be>
2017-03-20 11:42:52 +0100

    POST commentaar

--- src/Client/PostRequest.java
+++ src/Client/PostRequest.java
@@ -7,18 +7,45 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
+/**
+ * This class handles a POST request.
+ * 
+ * @author Laura Vranken
+ * @author Arne Vlietinck
+ */
 public class PostRequest {
-
+	
+	/**
+	 * Launch the post request. The response received from the HTTP server is displayed on the terminal. 
+	 * It is also saved in a HTMLfile locally.
+	 * 
+	 * @param 	hostAddress
+	 * 			The hostAddress of the address to connect with.
+	 * @param	afterSlash
+	 * 			The address after the slash of the address to connect with.
+	 * @param	port
+	 * 			The number of the port to connect with. 
+	 * @post	The input in the command line is read and is used as body for the post request.
+	 * @post	The post response of the requested site will be printed in the terminal.
+	 * @post	The post response of the requested site will be saved in a HTMLfile.
+	 * 			The conventions for the file are "POST" + hostAddress + ".html".
+	 * @throws	UnknownHostException
+	 * @throws	IOException
+	 */
 	public static void post(String hostAddress, String afterSlash, int port) throws UnknownHostException, IOException {
 
-		//verbinding leggen met site.
+		//Create socket
 		Socket s = new Socket(hostAddress, port);
-		//request
+		
+		//Request
 		PrintWriter pw = new PrintWriter(s.getOutputStream());
 		String sentence = "POST "+afterSlash+ " HTTP/1.1"+"\r\n" + "host: " + hostAddress + "\r\n" + "Content-Type: text/plain" + "\r\n" + "Content-Length: ";
 		System.out.print("Give Body:\r\n");
-
+		
+		//Input terminal
 		BufferedReader inputTerminal = new BufferedReader(new InputStreamReader(System.in));
+		
+		//Check the input
 		String contentTerminal;
 		if((contentTerminal = inputTerminal.readLine()) != null){
 			int length = contentTerminal.length();
@@ -30,14 +57,14 @@ public class PostRequest {
 		pw.print(sentence);
 		pw.flush();
 
-		//respons
+		//Respons
 		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-		//printwriter to htmlfile
+		//Printwriter to HTMLfile
 		PrintWriter out = new PrintWriter("POST"+ hostAddress +".html");
 		String commandLineString;
 
-		//printen respons
+		//Print respons
 		String htmlString = "";
 		while((commandLineString = br.readLine()) != null){
 			System.out.println(commandLineString);
@@ -46,12 +73,10 @@ public class PostRequest {
 			htmlString += commandLineString;
 		}
 
-		//sluiten na uitschrijven
+		//Close
 		br.close();
 		inputTerminal.close();
 		out.close();
-
-		//later sluiten
 		pw.close();
 		s.close();
 	}
